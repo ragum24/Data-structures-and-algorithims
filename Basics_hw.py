cf532320019e23519d39bf817f367a12033e7932
ragum24 <165797568+ragum24@users.noreply.github.com>
2026-02-17 20:14:26 +1100

    Add files via upload

---
+++ Basics_hw.py
@@ -0,0 +1,14 @@
+potato = []
+for i in range(10): 
+    numbers = input("Enter a number:")
+    potato.append(numbers)
+print(potato)
+
+#this is not adding a new number but assigning a new value (replacing)
+'''num = []
+immput = input("Input-")
+num = [immput]
+print(num)
+user = input("Input:")
+num = [user]
+print(num)'''
