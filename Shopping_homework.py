65372cd2881ebda572a3caec9cdfbb35825e3c77
ragum24 <165797568+ragum24@users.noreply.github.com>
2026-04-14 20:07:30 +1000

    Add files via upload

---
+++ Shopping_homework.py
@@ -0,0 +1,6 @@
+prices = [250, 499, 120, 300, 150] #each time the loops runs, item gives the next element
+#from the list and adds it to the total :)
+total = 0
+for item in prices: 
+    total = total+item
+    print(total)
