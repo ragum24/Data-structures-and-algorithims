04ea2483e1c9763a3dc41f07d9871f001a6bf1c9
ragum24 <165797568+ragum24@users.noreply.github.com>
2026-04-21 20:11:15 +1000

    Add files via upload

---
+++ for_loop_hw.py
@@ -0,0 +1,24 @@
+'''#1
+listed = [1,"eight",50,"egg.",":)",56000]
+for item in listed:
+    print(item)
+
+#2
+listing = [1,2,3,4,5,0]
+adding = 0
+for item in listing:
+    adding = item+adding
+    print(adding)'''
+
+#3
+listing_the_listed = [1,3,5,7,9,0,4]
+biggest = 0
+for item in listing_the_listed:  #This is an example of linear search
+    if biggest < item:
+        biggest = item
+    print(biggest)
+
+'''#4
+ive_run_out_of_names = [2,4,6,8,9,13,1]
+even = 0
+for item in ive_run_out_of_names:'''
